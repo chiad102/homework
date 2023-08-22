62bb3a7f9067c2a2c1d93f7ee701b408c695d91d
Чингис <cianeed08@gmail.com>
2023-08-22 20:23:24 +0300

    update ignore list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend - 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        System.out.println(boxer1 + boxer2);
+        System.out.println(boxer2 - boxer1);
+
+        System.out.println(boxer2 - boxer1);
+        System.out.println(boxer2 % boxer1);
+
+        var hours = 640;
+        var hoursForWorker = 8;
+        var workers = hours / hoursForWorker;
+        System.out.println(workers);
+
+        workers = workers + 94;
+        System.out.println("Если в компании работает " + workers + " человек, то всего часов " + workers * hoursForWorker);
     }
 }
